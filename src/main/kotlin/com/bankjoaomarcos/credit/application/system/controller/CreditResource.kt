85d854697da18c95cc5db242b5db9382db10714a
JJOAOMARCOSS <andramas.10@gmail.com>
2025-07-14 18:10:09 -0300

    :feat: Mundança para Tratamento de Exceção

--- src/main/kotlin/com/bankjoaomarcos/credit/application/system/controller/CreditResource.kt
+++ src/main/kotlin/com/bankjoaomarcos/credit/application/system/controller/CreditResource.kt
@@ -5,6 +5,7 @@ import com.bankjoaomarcos.credit.application.system.dto.CreditView
 import com.bankjoaomarcos.credit.application.system.dto.CreditViewList
 import com.bankjoaomarcos.credit.application.system.entity.Credit
 import com.bankjoaomarcos.credit.application.system.service.impl.CreditService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -25,7 +26,7 @@ class CreditResource(
 ) {
 
     @PostMapping
-    fun saveCredit(@RequestBody creditDto: CreditDto): ResponseEntity<String>{
+    fun saveCredit(@RequestBody @Valid creditDto: CreditDto): ResponseEntity<String>{
         val credit: Credit = this.creditService.save(creditDto.toEntity())
         return ResponseEntity.status(HttpStatus.CREATED)
             .body("Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!")
